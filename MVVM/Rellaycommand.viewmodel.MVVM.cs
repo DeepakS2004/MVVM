2c8c91fe9e36f93925120f584c72165392b2c2eb
Deepak S <149089221+DeepakS2004@users.noreply.github.com>
2024-08-30 13:23:27 +0530

    Add files via upload

---
+++ MVVM/Rellaycommand.viewmodel.MVVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace supermarket.viewmodel
+{
+    public class Rellaycommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action action;
+
+        public Rellaycommand(Action value)
+        { 
+            action = value;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            action();
+        }
+    }
+}
---
+++ Registeviewmodel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using supermarket.model;
+
+namespace supermarket.viewmodel
+{
+    public class Registeviewmodel 
+    {
+      
+        public Rellaycommand Registercommand {  get; set; }
+       
+        public Rellaycommand Clearcommand {  get; set; }
+
+        public Registermodel studitem { get; set; }
+
+
+        public Registeviewmodel()
+        {
+            studitem = studitem ?? new Registermodel();
+            studitem.Username = "Deepak";
+            studitem.Password = "1232";
+            studitem.Email = "dk@gmail.com";
+            studitem.Salary = "12000";
+            Registercommand = new Rellaycommand(Register);
+            Clearcommand = new Rellaycommand(Clear);
+            
+        }
+
+        public void Register()
+        { 
+        }
+        public void Clear() 
+        { 
+        }
+    }
+}
